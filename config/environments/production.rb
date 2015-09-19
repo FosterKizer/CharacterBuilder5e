c9c5df1ad3fe8acc21401bf6c40a04c6440ac84f
GoDucks713 <kizer713@gmail.com>
2015-09-19 03:43:30 +0000

    typo fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'character-builder-5e.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rpg-tools.herokuapp.com' }
 end
